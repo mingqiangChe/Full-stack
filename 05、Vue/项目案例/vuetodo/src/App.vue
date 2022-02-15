d4aaff6903837e142a00efaebbeda9c713b30f48
thomas373737 <cmq15650272396@163.com>
2022-02-15 11:13:19 +0800

    vuetodo

---
+++ 05、Vue/项目案例/vuetodo/src/App.vue
@@ -0,0 +1,97 @@
+<!-- 主体 -->
+<template>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :addTodo="addTodo"></MyHeader>
+        <MyList></MyList>
+        <MyFooter></MyFooter>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+import MyHeader from './components/MyHeader'
+import MyList from './components/MyList.vue'
+import MyFooter from './components/MyFooter.vue'
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: { MyHeader, MyList, MyFooter },
+  data() {
+    //这里存放数据
+    return {
+      todos: [
+        { id: '001', title: '抽烟', done: true },
+        { id: '002', title: '🍺', done: false },
+        { id: '003', title: '烫头', done: false },
+      ],
+    }
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj)
+      console.log(this.todos)
+    },
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+.btn:focus {
+  outline: none;
+}
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
