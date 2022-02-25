5098faecf4d063bada80a9a43500b550019cbfe2
thomas373737 <cmq15650272396@163.com>
2022-02-25 22:50:25 +0800

    11

--- 05、Vue/vue2/todo-vue-admin/src/router/index.js
+++ 05、Vue/vue2/todo-vue-admin/src/router/index.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: thomas373737
+ * @Date: 2022-02-25 12:52:32
+ * @LastEditors: thomas373737
+ * @LastEditTime: 2022-02-25 15:58:31
+ * @FilePath: /Full-stack/05、Vue/vue2/todo-vue-admin/src/router/index.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by 用户/公司名, All Rights Reserved. 
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
