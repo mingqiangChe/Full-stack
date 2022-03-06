9644608df7a464cef9d03e53ecb3f96b1a61fddf
thomas373737 <cmq15650272396@163.com>
2022-03-06 22:35:09 +0800

    新增架构方面

--- 05、Vue/项目案例/project_crm_huaqi/vue.config.js
+++ 05、Vue/项目案例/project_crm_huaqi/vue.config.js
@@ -8,15 +8,9 @@ function resolve(dir) {
 
 const name = defaultSettings.title || '后台管理系统' // page title
 
-// All configuration item explanations can be find in https://cli.vuejs.org/config/
+
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
+
   publicPath: '/hqtcsz_crm/',
   outputDir: 'dist',
   assetsDir: 'static',
