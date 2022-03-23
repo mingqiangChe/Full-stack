af7b2281035e386a8154552ef44183f60c95bcaa
mingqiangChe <cmq15650272396@163.com>
2022-03-23 12:53:11 +0800

    1

--- 05、Vue/项目案例/element/src/router/index.js
+++ 05、Vue/项目案例/element/src/router/index.js
@@ -1,44 +1,38 @@
-/*
- * @Author: your name
- * @Date: 2021-12-10 16:17:58
- * @LastEditTime: 2021-12-13 13:25:43
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: /Full-stack/05、Vue/element/src/router/index.js
- */
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Main from '../views/Main.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Main from "../views/Main.vue";
 // 解决面包屑点击首页报错
 const originPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
-  return originPush.call(this, location).catch(err => err)
-}
-Vue.use(VueRouter)
+  return originPush.call(this, location).catch((err) => err);
+};
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     // name: 'Main',
     component: Main,
     children: [
       {
-        path: '/',
-        name: 'home',
+        path: "/",
+        name: "home",
         // 组件懒加载
-        component: () => import('@/views/Home/Home')
-      }, {
-        path: '/mall',
-        name: 'mall',
+        component: () => import("@/views/Home/Home"),
+      },
+      {
+        path: "/mall",
+        name: "mall",
         // 组件懒加载
-        component: () => import('@/views/Mall/Mall')
-      }, {
-        path: '/user',
-        name: 'user',
+        component: () => import("@/views/Mall/Mall"),
+      },
+      {
+        path: "/user",
+        name: "user",
         // 组件懒加载
-        component: () => import('@/views/User/User')
-      }
-    ]
+        component: () => import("@/views/User/User"),
+      },
+    ],
   },
   // {
   //   path: '/about',
@@ -48,12 +42,12 @@ const routes = [
   //   // which is lazy-loaded when the route is visited.
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   // }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
