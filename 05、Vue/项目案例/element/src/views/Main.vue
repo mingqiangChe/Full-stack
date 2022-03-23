2c0afd9de04e05f9da00ea40febf0f6a7c6ea58e
mingqiangChe <cmq15650272396@163.com>
2022-03-23 12:56:24 +0800

    1

--- 05、Vue/项目案例/element/src/views/Main.vue
+++ 05、Vue/项目案例/element/src/views/Main.vue
@@ -1,40 +1,32 @@
 import Left from '@/components/Left.vue';
-<!--
- * @Author: your name
- * @Date: 2021-12-10 16:17:58
- * @LastEditTime: 2021-12-13 13:27:45
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: /Full-stack/05、Vue/element/src/views/Main.vue
--->
 <template>
+  <el-container>
+    <el-aside width="min-width:64px;">
+      <common-aside></common-aside>
+    </el-aside>
     <el-container>
-      <el-aside width="min-width:64px;">
-        <common-aside></common-aside>
-      </el-aside>
-      <el-container>
-        <el-header class="header"><common-header></common-header></el-header>
-        <!-- tag -->
-        <common-tag></common-tag>
-        <!-- 中间部分使用路由 -->
-        <el-main>
-          <router-view></router-view>
-        </el-main>
-      </el-container>
+      <el-header class="header"><common-header></common-header></el-header>
+      <!-- tag -->
+      <common-tag></common-tag>
+      <!-- 中间部分使用路由 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
+  </el-container>
 </template>
 
 <script>
 const CommonAside = () => import("../components/CommonAside.vue");
 const CommonHeader = () => import("../components/CommonHeader.vue");
-const CommonTag =()=>import("../components/CommonTag.vue")
+const CommonTag = () => import("../components/CommonTag.vue");
 // @ is an alias to /src
 export default {
   components: {
     CommonAside,
     CommonHeader,
-    CommonTag
-},
+    CommonTag,
+  },
 };
 </script>
 <style lang="scss" scoped>
