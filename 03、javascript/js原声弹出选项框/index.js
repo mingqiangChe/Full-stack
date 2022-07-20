544dfb6f23455a4365f814d0f3469c612f19c5c5
mingqiangChe <cmq15650272396@163.com>
2022-07-20 23:20:14 +0800

    格式化
    格式化

--- 03、javascript/js原声弹出选项框/index.js
+++
@@ -1,9 +0,0 @@
-const oneOppoMenu = function () {
-  this.event()
-}
-oneOppoMenu.prototype.event = function () {
-  window.addEventListener("click", function (e) {
-    console.log(e)
-  })
-}
-new oneOppoMenu()
--- 05、Vue/vue项目功能点实现方案.md
+++ 05、Vue/vue项目功能点实现方案.md
@@ -1,2 +1,5 @@
-# vue项目功能点实现方案
+# vue 项目功能点实现方案
 
+radio 下拉框等组件的绑定需要改成动态的：value
+form 表单如果循环渲染 那么可以将 form 表单绑定值为 item
+定制下拉框展示 可以在下拉框属性 :value 中绑定 “ ‘${item.value}(${item.label})‘ ” 类似这种格式
