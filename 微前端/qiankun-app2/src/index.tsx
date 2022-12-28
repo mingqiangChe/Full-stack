b8ed6b1af752144e3756cd34fd934cd74f74f6b3
mingqiangChe <cmq15650272396@163.com>
2022-12-28 22:11:34 +0800

    微前端

---
+++ 微前端/qiankun-app2/src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+export async function bootstrap() {
+  console.log('[react] react app bootstraped');
+}
+
+// @ts-ignore
+export async function mount(props) {//props 挂载元素
+  console.log(props);
+  ReactDOM.render(<App />, props.container ? props.container.querySelector('#root') : document.getElementById('root'));
+}
+// @ts-ignore
+export async function update(props) {//节点更新操作
+  console.log('update props ',props);
+}
+// @ts-ignore
+export async function unmount(props) {//取消挂载
+  ReactDOM.unmountComponentAtNode(props.container ? props.container.querySelector('#root') : document.getElementById('root'))
+}
+// @ts-ignore
+if (!window.__POWERED_BY_QIANKUN__) {//判断是否是乾坤框架 作单独渲染
+  ReactDOM.render(<App/>,document.getElementById('root'))
+}
+
+reportWebVitals();
