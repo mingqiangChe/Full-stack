56c79bda84e3c58edb8097765945c0d2210076dc
mingqiangChe <cmq15650272396@163.com>
2023-05-05 12:31:54 +0800

    up

---
+++ 05、Vue/vue2/代码/form-component/src/components/form/form-item.vue
@@ -0,0 +1,28 @@
+<!-- form-item.vue -->
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <div>
+      <slot></slot>
+    </div>
+    <h1>{{ apps }}pp</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "iFormItem",
+  inject: ["apps"],
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    prop: {
+      type: String,
+    },
+  },
+  mounted() {
+    console.log(this.apps, "ll")
+  },
+}
+</script>
---
+++ 05、Vue/vue2/代码/form-component/src/components/form/form.vue
@@ -0,0 +1,32 @@
+<!-- form.vue -->
+<template>
+  <form>
+    <slot></slot>
+  </form>
+</template>
+<script>
+export default {
+  name: "iForm",
+  props: {
+    model: {
+      type: Object,
+    },
+    rules: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      fields: [],
+    }
+  },
+  created() {
+    this.$on("on-form-item-add", (field) => {
+      if (field) this.fields.push(field)
+    })
+    this.$on("on-form-item-remove", (field) => {
+      if (field.prop) this.fields.splice(this.fields.indexOf(field), 1)
+    })
+  },
+}
+</script>
